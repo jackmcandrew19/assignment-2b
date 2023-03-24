e42742afae2b8d117dea15532e62825a7dedf96f
jackmcandrew19 <124379562+jackmcandrew19@users.noreply.github.com>
2023-03-24 09:55:05 -0400

    Update 2b.py

--- 2b.py
+++ 2b.py
@@ -1,28 +1,30 @@
 # TASK: calculates the area and circumference of the circle
 #diameter/2 = r
-#A = πr2
-#C = 2πr
+#A = πr2 or
+#C = 2πr or pi*D
 #Show A and C
 
 
-def getRadius(diameter):
+#def getRadius(diameter):
   # Write your function here
-  radius= diameter/2
-  return radius
-  
+ # radius = float(diameter) / 2
+ # return float(radius)
+ 
 def GetArea(radius):
   # Wrie your function here
-  Area = 3.14*(radius*radius)
-  return Area
-  
- def GetCirc(radius):
-  # Wrie your function here
-  Circ = 3.14*(radius*2)
+  Area = 3.14 *(radius * radius)
+  return float(Area)
+ 
+def GetCirc(radius):
+  Circ = 3.14 *(radius) * 2
   return Circ
-  
-  
+ 
+ 
 diameter = input("What is your diameter? ")
-print("Your radius is: ",getRadius(diameter))
+radius = float(diameter) / 2
+
+#print("Your radius is: ",getRadius(diameter))
 print("Your area is: ", GetArea(radius))
+
 print("Your Circumference is: ", GetCirc(radius))
 
